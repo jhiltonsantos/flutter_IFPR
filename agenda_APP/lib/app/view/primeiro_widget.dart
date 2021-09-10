bda17ccd07869036a4184e572dc788abfec0aaaf
Hilton Santos <jhilton930@gmail.com>
2021-09-10 15:41:34 -0300

    widget

---
+++ agenda_APP/lib/app/view/primeiro_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrimeiroWidget extends StatefulWidget {
+  const PrimeiroWidget({key}) : super(key: key);
+
+  @override
+  _PrimeiroWidgetState createState() => _PrimeiroWidgetState();
+}
+
+class _PrimeiroWidgetState extends State<PrimeiroWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
